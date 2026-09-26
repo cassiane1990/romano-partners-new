3ac972634ccf3a67bbcd7a31ed16cb697ec11306
cassiane1990 <bkdb7n92gw@privaterelay.appleid.com>
2026-09-26 04:02:05 +0200

    Restore stable service worker version 9f1cc9f

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="romano-pwa-v20";
+const CACHE="romano-pwa-v15";
 const CORE=["/","/index.html","/manifest.json","/icon.svg"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
